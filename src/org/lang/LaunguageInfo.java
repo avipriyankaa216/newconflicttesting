104760fa3877a0473cf85271ac499f13b7f7a317
avipriyankaa216 <priyankaaoct21@gmail.com>
2022-09-28 22:40:38 +0530

    first commit

---
+++ src/org/lang/LaunguageInfo.java
@@ -0,0 +1,20 @@
+package org.lang;
+
+public class LaunguageInfo {
+	public void tamilLanguage() {
+		// TODO Auto-generated method stub
+		System.out.println("the canditade has W/R knowledge in tamillaunguage");
+
+	}
+	public void englishLanguage() {
+		// TODO Auto-generated method stub
+		System.out.println("the canditade has W/R knowledge in Englishlaunguage");
+System.out.println("very");
+	}
+	public void hindiLaunguage() {
+		// TODO Auto-generated method stub
+		System.out.println("the canditade has W/R knowledge in hindilaunguage");
+  		
+	}
+
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,23 @@
+package org.lang;
+
+public class StateDetails {
+	private void southIndia() {
+		// TODO Auto-generated method stub
+		System.out.println("southindia");
+
+	}
+	private void northIndia() {
+		// TODO Auto-generated method stub
+System.out.println("northindia");
+	}
+public static void main(String[] args) {
+	LaunguageInfo i=new LaunguageInfo();
+	i.tamilLanguage();
+	i.englishLanguage();
+	i.hindiLaunguage();
+	StateDetails d=new StateDetails();
+	d.southIndia();
+	d.northIndia();
+	
+}
+}
